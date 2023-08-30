fae849e6f5b6e0739772674858049d3bab4d6dcd
wughangar <wughangar.rose@gmail.com>
2023-08-30 09:45:27 +0300

    file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,14 @@
 # configurations usings puppet
 
-file { '/root/.ssh':
-  ensure => directory,
-  mode   => '0700',
-}
-
 file{'/root/.ssh/config':
-ensure  => file,
-content => "Host server\n
+  ensure  => file,
+  content => "Host server\n
       IdentityFile ~/.ssh/school\n
       PasswordAuthentication no\n",
+  require => File['/root/.ssh'],
 }
+
+file{'/root/.ssh':
+  ensure => directory,
+}
+
