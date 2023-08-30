bfd473d471b29297a73037071aa29c56da6d6573
wughangar <wughangar.rose@gmail.com>
2023-08-30 06:18:30 +0000

    changed the interpretor to puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
-#!/usr/bin/env
+#!/usr/bin/puppet
 
 file { '/root/.ssh/config':
-ensure  => present,
-content => "Host 34.207.190.83\n IdentityFile ~/.ssh/school\n PasswordAuthentication no\n",}
+  ensure  => present,
+  content => "Host 34.207.190.83\n IdentityFile ~/.ssh/school\n PasswordAuthentication no\n",}
