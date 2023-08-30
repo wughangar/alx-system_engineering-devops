2cf5743eace8e3ca66ab82f4ef13188fa8c52678
wughangar <wughangar.rose@gmail.com>
2023-08-30 18:57:25 +0300

    updated manifest correctly

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,11 +4,11 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'aaaaa':
+file_line { 'rose':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=mBB_dB3twE4&t=1123s permanent;',
 }
 
 file { '/var/www/html/index.html':
