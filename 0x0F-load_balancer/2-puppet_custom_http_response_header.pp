4f0e7802a8684510dcedf3116a2b999e9480b1d4
wughangar <wughangar.rose@gmail.com>
2023-09-05 12:47:20 +0000

    file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,6 +3,14 @@ package { 'nginx':
   ensure => 'installed',
 }
 
+# Modify Nginx configuration to add custom HTTP header
+exec { 'configure_nginx':
+  command => "sed -i '/listen 80 default_server;/a add_header X-Served-By \$HOSTNAME;' /etc/nginx/sites-available/default",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+
 # http header response
 file { '/etc/nginx/sites-available/default':
   ensure  => 'file',
