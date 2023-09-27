971270d854bed0a9a1aa103b9b97fe17173f069f
wughangar <wughangar.rose@gmail.com>
2023-09-27 04:22:35 +0000

    employee to do list

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,46 +10,33 @@ import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
+def get_employee_todo_list(employee_id):
     """
-    Retrieve and display an employee's TODO list progress
-    Args:
-        employee_id (int): The ID of the employee
-        whose progress is to be retrieved.
-    This function makes API requests to fetch
-    information about the employee's TODO list,
-    calculates the number of completed tasks, and prints the progress.
-
-    Raises:
-        requests.exceptions.RequestException: If the API request fails.
-
-    Example:
-        get_employee_todo_progress(2)
+    function to retrive the to do list
     """
     base_url = "https://jsonplaceholder.typicode.com"
-    user_response = requests.get(f"{base_url}/users/{employee_id}")
-    user_data = user_response.json()
+    todo_url = f"{base_url}/todos?userId={employee_id}"
 
-    todo_response = requests.get(f"{base_url}/todos?userId={employee_id}")
-    todo_data = todo_response.json()
+    response = requests.get(todo_url)
 
-    total_tasks = len(todo_data)
-    completed_tasks = sum(1 for task in todo_data if task["completed"])
+    if response.status_code == 200:
+        todos = response.json()
+        total_tasks = len(todos)
+        completed_tasks = sum(1 for todo in todos if todo['completed'])
 
-    print(f"Employee {user_data['name']} is done with
-          tasks({completed_tasks}/{total_tasks}): ")
-    for task in todo_data:
-        if task["completed"]:
-            print(f"\t{task['title']}")
+        print(f"Employee EMPLOYEE_NAME is done with
+              tasks({completed_tasks}/{total_tasks}): ")
+        for todo in todos:
+            if todo['completed']:
+                print(f"\t{todo['title']}")
+    else:
+        print(f"Error: Unable to fetch data for employee {employee_id}")
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
+        print("Usage: python employee_todo.py <employee_id>")
         sys.exit(1)
 
-    try:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
-    except ValueError:
-        print("Invalid input")
+    employee_id = int(sys.argv[1])
+    get_employee_todo_list(employee_id)
