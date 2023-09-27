ccbe29ed0339581daf669e030e8e6ec5ab2ad0a1
wughangar <wughangar.rose@gmail.com>
2023-09-27 05:56:00 +0000

    style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-'''
+"""
 Gather data from an API
-'''
+"""
 import requests
-import sys import argv
+from sys import argv
 
 
 def get_employee_todo_list(employee_id):
-    '''
+    """
     function to retrive the to do list
-    '''
+    """
     base_url = "https://jsonplaceholder.typicode.com"
     todo_url = f"{base_url}/todos?userId={employee_id}"
 
@@ -18,21 +18,23 @@ def get_employee_todo_list(employee_id):
     if response.status_code == 200:
         todos = response.json()
         total_tasks = len(todos)
-        completed_tasks = sum(1 for todo in todos if todo['completed'])
+        completed_tasks = sum(1 for todo in todos if todo["completed"])
 
-        print(f"Employee EMPLOYEE_NAME is done with
-              tasks({completed_tasks}/{total_tasks}): ")
+        print(
+            f"Employee {name} is done with "
+            f"tasks({completed_tasks}/{total_tasks}): "
+        )
         for todo in todos:
-            if todo['completed']:
+            if todo["completed"]:
                 print(f"\t{todo['title']}")
     else:
         print(f"Error: Unable to fetch data for employee {employee_id}")
 
 
 if __name__ == "__main__":
-    '''
+    """
     main function
-    '''
+    """
     if len(sys.argv) != 2:
         print("Usage: python employee_todo.py <employee_id>")
         sys.exit(1)
