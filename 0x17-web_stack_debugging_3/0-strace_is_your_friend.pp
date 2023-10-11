517aad76e16969e6c728b309fb6d20322a36a98f
wughangar <wughangar.rose@gmail.com>
2023-10-11 15:44:54 +0300

    test

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,6 @@
 # manifest that dowloads apache to fix 500 error
 
-class apache_install {
-package { 'apache2':
-ensure => 'installed',
-}
-
-service { 'apache2':
-ensure  => 'running',
-enable  => true,
-require => Package['apache2'],
-}
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
