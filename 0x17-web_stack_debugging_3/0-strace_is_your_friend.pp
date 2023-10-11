2173d347a69b82aa43fe05a9a85747768a71ad1a
wughangar <wughangar.rose@gmail.com>
2023-10-11 15:46:46 +0300

    lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,13 @@
 # manifest that dowloads apache to fix 500 error
 
-exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
+file { '/var/www/html/wp-settings.php':
+  ensure  => file,
+  content => file('/var/www/html/wp-settings.php').content.gsub('.phpp', '.php'),
+  notify  => Exec['Reload Apache'],
+}
+
+exec { 'Reload Apache':
+  command     => 'systemctl reload apache2',
+  refreshonly => true,
+  subscribe   => File['/var/www/html/wp-settings.php'],
 }
