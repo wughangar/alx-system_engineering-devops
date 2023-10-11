0406ff5e5ed3543b11f43db4b374721310a42298
wughangar <wughangar.rose@gmail.com>
2023-10-11 15:58:11 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # manifest that dowloads apache to fix 500 error
 
 exce{ 'fixing wordpress error':
-command => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+command  => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+provider => shell
 }
