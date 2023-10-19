b19c4835bc3899a448968d0e495911839363409b
wughangar <wughangar.rose@gmail.com>
2023-10-20 02:35:07 +0300

    holberton user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,32 @@
+# Create the holberton user
+user { 'holberton':
+  ensure => 'present',
+  managehome => true,
+  home => '/home/holberton',
+}
+
+user { 'holberton':
+  password => 'holberton',
+}
+
+file { '/etc/sudoers.d/holberton':
+  ensure  => 'file',
+  content => "holberton ALL=(ALL) NOPASSWD: ALL\n",
+  mode    => '0440',
+  require => User['holberton'],
+}
+
+file { '/home/holberton':
+  ensure => 'directory',
+  owner  => 'holberton',
+  group  => 'holberton',
+  mode   => '0700',
+}
+
+file { '/home/holberton/somefile.txt':
+  ensure => 'file',
+  owner  => 'holberton',
+  group  => 'holberton',
+  mode   => '0600',
+  content => 'This is some content.',
+}
